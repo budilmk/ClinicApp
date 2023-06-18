12f2c04ce35e798be1d940fa17f7edcd9e8c91d8
budilmk <budilmk@gmail.com>
2023-06-18 10:14:56 +0800

    Created Entities

---
+++ Entities/Appointment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicApp.Entities
+{
+    public class Appointment
+    {
+        [Key] public Guid Id { get; set; }
+        [Key] public Guid SlotId { get; set; }
+        public Guid PatientId { get; set; }
+        public string? PatientName { get; set; }
+        public DateTime ReservedAt { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
---
+++ Entities/Slot.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicApp.Entities
+{
+    public class Slot
+    {
+        [Key] public Guid Id { get; set; }
+        public string Time { get; set; }
+        public Guid DoctorId { get; set; }
+        [Key] public string? DoctorName { get; set; }
+        public bool? IsReserved { get; set; }
+        public decimal? Cost { get; set; }
+
+    }
+}
