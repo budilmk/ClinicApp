14b033b5fea512f59c252dbb90b9b41919e8efe5
budilmk <budilmk@gmail.com>
2023-06-18 11:55:51 +0800

    Tested Slot request, created slot

--- Services/SlotService.cs
+++ Services/SlotService.cs
@@ -1,16 +1,24 @@
 ﻿using ClinicApp.Entities;
+using ClinicApp.Repositories;
 using ClinicApp.Services;
 namespace ClinicApp
     .Services
 {
     public class SlotService : ISlotService
     {
+        private readonly ISlotRepository _slotRepository;
+        public SlotService(ISlotRepository slotRepository)
+        {
+            _slotRepository = slotRepository;
+        }
         public async Task CreateSlot(string time, string doctorName, decimal cost)
         {
             if (string.IsNullOrEmpty(doctorName))
             {
 
             }
+            var slot = new Slot { Id = Guid.NewGuid(), Time = time, DoctorId = Guid.NewGuid(), DoctorName = doctorName, Cost = cost, IsReserved = false };
+            await _slotRepository.Add(slot);
 
         }
 
