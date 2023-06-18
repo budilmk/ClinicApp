4614598fa8c2c3473403f47466c1eb671620e04a
budilmk <budilmk@gmail.com>
2023-06-18 10:59:31 +0800

    Created Db config

---
+++ Database/Class.cs
@@ -0,0 +1,16 @@
+﻿using ClinicApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Test1.Database;
+
+public class AppointmentConfig : IEntityTypeConfiguration<Appointment>
+{
+    public void Configure(EntityTypeBuilder<Appointment> builder)
+    {
+        builder.ToTable("Appointments");
+        builder.HasKey(x => x.SlotId);
+        builder.HasKey(x => x.Id);
+        //throw new NotImplementedException();
+    }
+}
---
+++ Database/SlotConfig.cs
@@ -0,0 +1,17 @@
+﻿using ClinicApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ClinicApp.Entities;
+
+namespace Test1.Database;
+
+public class SlotConfig : IEntityTypeConfiguration<Slot>
+{
+    public void Configure(EntityTypeBuilder<Slot> builder)
+    {
+        builder.ToTable("Slots");
+        builder.HasKey(x => x.DoctorName);
+        builder.HasKey(x => x.Id);
+        //throw new NotImplementedException();
+    }
+}
